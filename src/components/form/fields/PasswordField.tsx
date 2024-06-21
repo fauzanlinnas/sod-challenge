776c3d43266bfdfdb535da5b81f91585354f7e20
fauzan <fauzanlinnas@gmail.com>
2024-06-21 14:55:35 +0700

    feat: add password visibility icon

--- src/components/form/fields/PasswordField.tsx
+++ src/components/form/fields/PasswordField.tsx
@@ -1,10 +1,14 @@
 // TODO: Challenge #3 - create a password field component
+import EyeInvisible from "@/assets/icons/EyeInvisible";
+import EyeVisible from "@/assets/icons/EyeVisible";
 import { PasswordFieldProps } from "@/components/form/types";
 import { Input } from "@/components/ui/input";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const PasswordField = React.forwardRef<HTMLInputElement, PasswordFieldProps>(
   ({ field, value, path, updateModelValue, ...props }, ref) => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
     useEffect(() => {
       console.log(`PasswordField ${path} mounted`);
       return () => {
@@ -16,17 +20,30 @@ const PasswordField = React.forwardRef<HTMLInputElement, PasswordFieldProps>(
       console.log(`PasswordField ${path} rerendered`);
     });
 
+    const togglePasswordVisibility = () => {
+      setIsPasswordVisible(!isPasswordVisible);
+    };
+
     return (
-      <Input
-        ref={ref}
-        name={field.name}
-        id={path}
-        // @ts-ignore
-        value={value}
-        onChange={(e) => updateModelValue(path, field, e.target.value)}
-        type="password"
-        {...props}
-      />
+      <div className="relative">
+        <Input
+          ref={ref}
+          name={field.name}
+          id={path}
+          // @ts-ignore
+          value={value}
+          onChange={(e) => updateModelValue(path, field, e.target.value)}
+          type={isPasswordVisible ? "text" : "password"}
+          {...props}
+        />
+        <button
+          type="button"
+          onClick={togglePasswordVisibility}
+          className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500"
+        >
+          {isPasswordVisible ? <EyeInvisible /> : <EyeVisible />}
+        </button>
+      </div>
     );
   }
 );
